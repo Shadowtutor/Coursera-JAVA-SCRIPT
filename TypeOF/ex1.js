289ea3d80cc4016cb86aa689699eac822c3c886c
Gilvan Daniel <161324866+Shadowtutor@users.noreply.github.com>
2025-01-13 15:57:28 -0400

    Add files via upload

---
+++ TypeOF/ex1.js
@@ -0,0 +1,12 @@
+/*Using typeof*/
+
+//var test = typeof('wha is this');
+//var test = typeof(10);
+//var test = typeof(3.14);
+//var test = typeof(true);
+//var test = typeof(false);
+//var test = typeof(1<2);
+//var test = typeof([1,2,3]);
+//var test = typeof({firstProperty: 1});
+//var test = typeof(function abc() {console.log('abs');});
+console.log(test);
